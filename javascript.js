6e478c8272188b0ddd524571362b186ca468ad2c
Alex <alexviil145@live.com>
2018-11-28 22:08:41 +0200

    dünaamilisem leht, javascript lisatud, mille abil paragraafid peituvad/näitavad ennast kui peale vajutada

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 function tutvustus() {
 	var nimi = prompt("Palun sisestage oma nimi", "Mari Mets");
-	if (nimi != null) {
+	if (nimi != "" && nimi !=null) {
 		document.getElementById("tervitus").innerHTML = "Tere " + nimi + "! Loodame et leiate siit lehelt palju kasulikku!";
+	}
+}
+
+function peida(id) {
+	var x = document.getElementById(id)
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
 	}
 }
